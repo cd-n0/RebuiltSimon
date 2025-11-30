993d9572a9655bd5e6d6cdd258bd252e35ada360
cdn0 <59338209+cd-n0@users.noreply.github.com>
2025-11-30 13:10:32 +0300

    Check if we are in game before attempting to disable stamina

--- RebuiltSimon/Features/Cheats/disable_stamina.c
+++ RebuiltSimon/Features/Cheats/disable_stamina.c
@@ -1,7 +1,9 @@
 #include "RebuiltSimon/globals.h"
 #include "RebuiltSimon/cvars.h"
+#include "RebuiltSimon/SDK/Helpers/Entities/player.h"
 
 void disable_stamina() {
+    if (!in_game()) return;
     if (g_CoF.gsv_player) {
         edict_t** dict = g_CoF.gsv_player;
         if (dict) {
